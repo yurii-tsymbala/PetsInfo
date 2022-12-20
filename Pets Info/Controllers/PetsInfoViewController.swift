5d9db565ecf24a1057e412de3ff12caebbbbf545
Yurii Tsymbala <zumbala2013@gmail.com>
2022-12-20 14:28:38 +0200

    Add PetsInfoViewController

---
+++ Pets Info/Controllers/PetsInfoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PetsInfoViewController.swift
+//  Pets Info
+//
+//  Created by Yurii on 20.12.2022.
+//
+
+import UIKit
+
+class PetsInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
